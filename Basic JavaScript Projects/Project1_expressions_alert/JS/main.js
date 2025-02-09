9a97c6c36099969d673dcaefa3dffb303f7feec6
Lemar85 <lemar.leach@gmail.com>
2025-02-09 02:14:44 -0500

    Basic JS Project Submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,31 @@
+window.alert("Hello, world!"); //This is an example of how to use the window.alert method
+document.write('Hello, world!<br><br>'); // This is an eample of how to use the document.write() method
+
+var A = "My name is Lemar<br><br>"; //This assigns a value to var A
+document.write(A);
+
+document.write(" \"I don't think anybody has a ceiling. Everybody can constantly improve.\"<br> -Chip Kelly<br><br>")
+
+document.write("\"Be who you are and say what you feel,"
+    +" because those who mind don\'t matter and thos who matter don\'t mind\""
+    +"-Dr. Seuss<br><br>"); //This is an example of how to make quotation marks appear on screen and not be part of the syntax
+
+var B = "\"Yeah I\'m fine if F is for feeling overwhelmed,"
++" and I is for I\'m not alright<br>"
++"Yeah I\'m fine if N is for not being able to sleep"
++" E for every night<br>"
++"-Kyle Hume - Fine<br><br>"
+document.write(B);
+
+var Aunt="Adriene", Cousin="Elijah", Host="Lemar"; //Assigns a value to the variables "Aunt", "Cousin" and "Host"
+document.write(Aunt);
+
+document.write(19 + 85);
+
+var Sent1 = "This is the beginning of the string" //assigns variable Sent1
+var Sent2 = "and this is the end of the string" //assigns variable Sent2
+
+document.write("<br><br>This is the beginning of the string"
+    +" and this is the end of the string<br><br>"); //This concatenates the variables Sent1 and Sent2 together
+
+document.write(Sent1 + Sent2); //This is an expression adding Sent1 to Sent2
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/string_assignment.js
@@ -0,0 +1,3 @@
+var A = "Lemar likes puzzles";
+document.write('Lemar likes puzzles');
+window.alert("Lemar likes puzzles")
