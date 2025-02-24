eac39684563a64159fdb7867fd24bd21aeb049cf
Lemar85 <lemar.leach@gmail.com>
2025-02-24 02:37:05 -0500

    code corrections

--- Basic JavaScript Projects/Project7_scope_time_function/JS/P7_Submission_Assignment.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/P7_Submission_Assignment.js
@@ -43,10 +43,10 @@ function jack_function() {
 function Time_function() {
     var Time = new Date().getHours();
     var Reply;
-    if (Time < 12 == Time > 0) {
+    if (Time < 12 && Time > 0) {
         Reply = "It is morning time!";
     }
-    else if (Time >= 12 == Time < 18) {
+    else if (Time >= 12 && Time < 18) {
         Reply = "It is afternoon.";
     }
     else {
