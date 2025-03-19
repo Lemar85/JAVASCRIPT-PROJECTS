9c20750e9a66b867a8b04ae6fa11aa2efb34fe90
Lemar85 <lemar.leach@gmail.com>
2025-03-19 00:05:31 -0400

    one-page website upload

--- One-Page-Website/JS/one_page_website.js
+++ One-Page-Website/JS/one_page_website.js
@@ -1,9 +1,7 @@
-//Open the Modal
 function openModal() {
     document.getElementById("myModal").style.display = "block";
 }
 
-//Close the Modal
 function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
@@ -11,12 +9,10 @@ function closeModal() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-//Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -30,9 +26,6 @@ function showSlides(n) {
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
